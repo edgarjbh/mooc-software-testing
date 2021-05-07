ae936b7bd33e4764ad4f9eb376a1a6af27c108e6
Edgar Balladares <edgarjbh@gmail.com>
2021-05-07 14:40:31 -0500

    Finished GHappy excercise

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,23 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    @ParameterizedTest(name = "string={0}, result={1}")
+    @CsvSource({"xxggxx,true", "xxgxx,false", "xgxxx,false", "ggx,true", "gx,false"})
+    public void testGhappyOneOccurrence(String word, boolean expected) {
+        GHappy hap = new GHappy();
+        Assertions.assertEquals(expected, hap.gHappy(word));
+    }
+
+    @ParameterizedTest(name = "string={0}, result={1}")
+    @CsvSource({"xxggxxggww,true", "ggxxddgggcgchh,false", "ggxxxggxgg,true"})
+    public void testGhappyMoreThanOneOccurrence(String word, boolean expected) {
+        GHappy hap = new GHappy();
+        Assertions.assertEquals(expected, hap.gHappy(word));
+    }
+
 }
